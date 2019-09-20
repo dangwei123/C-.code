c4a72bf61a9eaa06de73816d30e2543acc210e9d
党威 <1357711677@qq.com>
2019-09-20 20:30:23 +0800

    2019.9.20--2.c

---
+++ 2019.9.20--2.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+int getSum(int a, int b)
+{
+	int c = 0;
+	while (b)
+	{
+		c = ((unsigned int)a&b) << 1;
+		a = a^b;
+		b = c;
+	}
+	return a;
+}
+int main()
+{
+	int ret=getSum(-3,7);
+	printf("%d\n", ret);
+	return 0;
+}
